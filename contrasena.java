8758527c785e1b302e90008181f9d7eadaf497d2
airlegend111 <128436131+airlegend111@users.noreply.github.com>
2023-08-18 10:46:03 -0500

    version2

--- contrasena.java
+++ contrasena.java
@@ -7,18 +7,21 @@ import java.util.Scanner;
  * contrasena
  */
 public class contrasena {
+
     public static void main(String[] args) {
-         final String  contrasenabase = "15689";
+         final String  CONTRASENABASE = "15689";
+         final String  USERNAME = "Baena";
         String  usuario, contrasena;
     Scanner scanner = new Scanner(System.in);
-    System.out.println("Digita tu nombre de usuario");
-    usuario = scanner.next();
+    
     do {
+        System.out.println("Digita tu nombre de usuario");
+        usuario = scanner.nextLine();
         System.out.println("Digite la contraseña del sistema");
-        contrasena = scanner.next();
+        contrasena = scanner.nextLine();
 
         
-    } while (contrasena.equals(contrasenabase) == false);
+    } while ((contrasena.equals(CONTRASENABASE) == false) && (usuario.equals(USERNAME) == false) );
     scanner.close();
      System.out.println("Bienvenido al sistema usuario");
 
